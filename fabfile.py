2596656d52bc6711776da3d05bad2766bbdfc5dc
Liza Daly <lizadaly@gmail.com>
2016-03-07 15:21:26 -0500

    Actually don't wait that long

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ from fabric.api import local, settings, abort, run, cd, env, put, sudo
 from fabric.contrib.console import confirm
 
 import time
+DEPLOY_WAIT_TIME = 15
 
 timestamp="release-%s" % int(time.time() * 1000)
 
@@ -11,6 +12,7 @@ env.user = 'deploy'  # Special group with limited sudo
 env.hosts = ['104.236.224.252']
 
 code_dir = '/home/liza/scribeAPI'
+
 
 def deploy():
     deploy_app()
@@ -21,6 +23,6 @@ def deploy_app():
         run('git pull origin master')
         run('rake project:load["label_this","workflows","content"]')
     sudo('service unicorn_labelthis stop', shell=False)
-    time.sleep(30)  # Wait for the process to die
+    time.sleep(DEPLOY_WAIT_TIME)  # Wait for the process to die
     sudo('service unicorn_labelthis start', shell=False)
     print "Done deploying"
