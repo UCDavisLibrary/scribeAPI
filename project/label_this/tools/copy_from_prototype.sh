94539c39d3298153ab41a632825f7d33eb7d45f4
Liza Daly <lizadaly@gmail.com>
2016-03-28 18:25:13 -0400

    Import separate SCSS file

--- project/label_this/tools/copy_from_prototype.sh
+++ project/label_this/tools/copy_from_prototype.sh
@@ -1,4 +1,5 @@
 cp ../../../../ucd-lib-prototypes/source/scss/_settings.scss ../assets/scss/
+cp ../../../../ucd-lib-prototypes/source/scss/_labelthis-styles.scss ../assets/scss/
 cp ../../../../ucd-lib-prototypes/source/scss/app.scss ../assets/scss/
 cp ../../../../ucd-lib-prototypes/public/images/* ../assets/images/
 
