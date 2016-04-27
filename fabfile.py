2d17236af067022c9c3fe8ef6f62e62d16bb7eec
Liza Daly <lizadaly@gmail.com>
2016-04-27 14:00:53 -0400

    Have deployment auto-refresh static resources

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ import os
 
 from fabric.api import local, settings, abort, run, cd, env, put, sudo
 from fabric.contrib.console import confirm
+import requests
 
 import time
 DEPLOY_WAIT_TIME = 15
@@ -15,6 +16,7 @@ env.hosts = ['labelthis.lib.ucdavis.edu']
 #code_dir = '/home/liza/scribeAPI'
 code_dir = '/home/deploy/scribeAPI'
 
+PROD_URL = "https://" + env.hosts[0]
 
 def deploy():
     deploy_app()
@@ -28,6 +30,7 @@ def deploy_app():
     stop_host()
     time.sleep(DEPLOY_WAIT_TIME)  # Wait for the process to die
     start_host()
+    ping_host()
     
     print("Done deploying")
 
@@ -38,3 +41,7 @@ def start_host():
     sudo('service unicorn_labelthis start', shell=False)
 
 
+def ping_host():
+    """Ping the home page to start the refresh cycle for the static resources"""
+    print("Requesting the home page to refresh static resources")
+    requests.get(PROD_URL)
