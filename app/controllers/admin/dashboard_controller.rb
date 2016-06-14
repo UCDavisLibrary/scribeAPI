444a76766a8c5197b02260dbe0e58e4df112017d
Liza Daly <lizadaly@gmail.com>
2016-06-14 15:03:33 -0400

    Improve logic for verify/approve

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,17 +5,18 @@ class Admin::DashboardController < Admin::AdminBaseController
     limit = 5
     page = get_int :page, 1
 
-    # Verify items
-    @verifies = Subject.page(page).per(limit).active_non_root.by_workflow(Workflow.where(:name=>"verify").first.id)
+    # Items that are in the Verify workflow and have no child subjects
+    @verifies = Subject.page(page).per(limit).active_non_root.by_workflow(Workflow.where(:name=>"verify").first.id).where(:secondary_subject_count => 0)
 
-    # Filter on labels that are not Approved
+    # Filter on labels that are not Approved but all Verified children have secondary subjects
     retired_subject_ids = Subject.root.retired().map{|s| s.subject_set_id }
-    approvables = Subject.active_non_root.where(:subject_set_id.nin => retired_subject_ids,
-                                              :workflow => Workflow.where(:name=>"verify").first.id)
-
+    approvables = Subject.where(:subject_set_id.nin => retired_subject_ids).or({type: "consensus_image"}, {type: "consensus_text"})
     approvable_map = {}
     for subject in approvables
-      approvable_map[subject.subject_set_id] = subject
+      related_subjects = Subject.by_subject_set(subject.parent_subject.subject_set_id).by_workflow(Workflow.where(:name=>"verify").first.id).where(:secondary_subject_count => 0)
+      if related_subjects.count == 0
+        approvable_map[subject.subject_set_id] = subject
+      end
     end
     @approves = Kaminari.paginate_array(approvable_map.values)
     @stats = project.calc_stats
